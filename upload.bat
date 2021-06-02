646d8c862f8e215f6b4453da7a766cf7b2bd29ca
MCorange99 <79301995+MCorange99@users.noreply.github.com>
2021-06-02 17:16:21 +0300

    make it better

--- upload.bat
+++ upload.bat
@@ -1,25 +1,4 @@
-@echo off
 
-"C:\Program Files (x86)\WinSCP\WinSCP.com" ^
-  /log="C:\writable\path\to\log\WinSCP.log" /ini=nul ^
-  /command ^
-    "open sftp://zmbxfyh.534cdfcb:20050628@de15.falix.gg:2022/ -hostkey=""ssh-rsa 2048 uJmkfb8kSz/qVJR+mIbOSj/z8yo7DvbkA8PZXMUV3hw="" -rawsettings FSProtocol=2" ^
-    "lcd C:\Users\mazas\Desktop\keithos_v3" ^
-    "cd /" ^
-    "put *" ^
-    "exit"
-
-set WINSCP_RESULT=%ERRORLEVEL%
-if %WINSCP_RESULT% equ 0 (
-  echo Success
-) else (
-  echo Error
-)
-
-pause
-
-exit /b %WINSCP_RESULT%
-
-
-
-
+git add .
+git commit -am "make it better"
+git push heroku master
